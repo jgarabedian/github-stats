d83ef29cc6ee0a398ec48724367ed372d6968be0
Jack Garabedian <jgarabedian96@gmail.com>
2020-11-25 09:16:57 -0500

    use dotenv local and add more stats

--- server.py
+++ server.py
@@ -2,9 +2,11 @@ from flask import Flask, render_template
 from github import Github
 import pprint
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 
-g = Github(os.environ.get('GITHUB_AUTH'))
+g = Github(os.getenv('GITHUB_AUTH'))
 
 app = Flask(__name__)
 
@@ -13,7 +15,7 @@ def index():
 	user = g.get_user()
 	repos = g.get_user().get_repos(sort="updated", visibility="public")
 	# sorted_repos = sorted(list(repos), key=lambda k: k['updated_at'], reverse=True)
-	# pprint.pprint(repos[0].__dict__)
+	# pprint.pprint(vars(repos[1]))
 	# r_list = [vars(r) for r in repos]
 
 	# pprint.pprint(r_list[0])
